1c73c5d8280d1486fdc46b0a8fa1823f02a04664
Zakharov <zakharov.products@gmail.com>
2022-01-16 11:55:23 +0300

    made_an_element_of_the_catalog_of_heart_rate_monitors

--- src/js/main.js
+++ src/js/main.js
@@ -14,3 +14,38 @@ $(document).ready(function(){
         ]
     });
 });
+
+function activeTabs() {
+    let tabs = document.querySelectorAll('.tabs__item');
+    let childrenTabs = document.querySelectorAll('.tabs__link');
+    for (let tab of tabs) {
+        tab.addEventListener('click', (event) => {
+            for (let tab of tabs) {
+                tab.classList.remove('tabs__item_active');
+            }
+
+            for (let childTabs of childrenTabs) {
+                childTabs.classList.remove('tabs__link_active');
+            }
+
+            if (!event.currentTarget.classList.contains('tabs__item_active')) {
+                event.currentTarget.classList.add('tabs__item_active');
+                event.target.classList.add('tabs__link_active');
+            }
+        });
+    }
+}
+
+activeTabs();
+
+function showSubstrate() {
+    document.querySelector('.catalog__item-more').addEventListener('click', (event) => {
+        document.querySelector('.item-substrate').classList.add('item-substrate_active');
+    });
+
+    document.querySelector('.button-back').addEventListener('click', () => {
+        document.querySelector('.item-substrate').classList.remove('item-substrate_active');
+    })
+}
+
+showSubstrate();
